197e5c5b5953b83b8e91cffea0c8b4110c465291
MrPloman <polplanaayet@gmail.com>
2026-07-21 14:10:00 +0200

    Feature[Unit Test in Package]: Card tests implemented

---
+++ packages/ui/src/card/Card.test.tsx
@@ -0,0 +1,54 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Card } from "./Card";
+describe("Card", () => {
+    it("Shows title with first letter capitalized", () => {
+        render(
+            <Card
+                title="pikachu"
+                img={{ src: "pikachu.jpg", alt: "Pikachu" }}
+                id="1"
+                badges={[{ color: "#EE8130", label: "Fire" }]}
+                buttons={[{ label: "Click me", onClick: () => {} }]}
+            />,
+        );
+        expect(screen.getByText(/Pikachu/)).toBeInTheDocument();
+    });
+
+    it("Render badges for each type", () => {
+        render(
+            <Card
+                title="pikachu"
+                img={{ src: "pikachu.jpg", alt: "Pikachu" }}
+                id="1"
+                badges={[
+                    { color: "#EE8130", label: "Fire" },
+                    { color: "#A8A77A", label: "Normal" },
+                ]}
+                buttons={[{ label: "Click me", onClick: () => {} }]}
+            />,
+        );
+        const allButtons = screen.getAllByRole("button");
+        const badgeButtons = allButtons.filter((btn) => btn.hasAttribute("aria-pressed"));
+
+        expect(badgeButtons).toHaveLength(2);
+    });
+
+    it("Render image with correct alt text", () => {
+        render(
+            <Card
+                title="pikachu"
+                img={{ src: "pikachu.jpg", alt: "Pikachu" }}
+                id="1"
+                badges={[
+                    { color: "#EE8130", label: "Fire" },
+                    { color: "#A8A77A", label: "Normal" },
+                ]}
+                buttons={[{ label: "Click me", onClick: () => {} }]}
+            />,
+        );
+        const image = screen.getByRole("img", { name: "Pikachu" });
+        expect(image).toHaveAttribute("alt", "Pikachu");
+    });
+});
